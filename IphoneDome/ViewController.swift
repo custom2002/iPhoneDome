a76aa7ff548e2e0bb80256877319a520e8541d69
zhangjianli <Noregret0210@icloud.com>
2017-04-27 10:58:08 +0800

    Initial Commit

---
+++ IphoneDome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IphoneDome
+//
+//  Created by 张坚力 on 2017/4/27.
+//  Copyright © 2017年 张坚力. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
